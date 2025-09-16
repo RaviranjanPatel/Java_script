d843ba4d29eca6b25fae6a93366ed0884624676a
RaviranjanPatel <kraviranjan2004@gmail.com>
2025-09-16 15:27:15 +0530

    commit

---
+++ FSD/js/day_6/day6.js
@@ -0,0 +1,15 @@
+// function Output(){
+//     var x=y=8;
+// }
+// Output();
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+// let a = "235"-23+23+true-false+23-235+235;
+// console.log(a);
+
+// let a = "235"-23+23+"true"-false+23-235+235;
+// console.log(a);
+
+
---
+++ FSD/js/index.js
@@ -0,0 +1,88 @@
+//Output method
+// console.log("FSD class");
+// Input Method
+// let a=prompt("Enter a number:");
+//console.log=(a)
+
+// var a=20;
+// var a=30;  //can be redeclear 
+
+// let a=40;
+// console.log("After first assignment of b's Value" +b);
+// a=70;
+// console.log("After first assignment of b's Value" +b);
+// {
+//     let name ="Ravi"
+//     name="bhjk"
+//     console.log("name:" +name);
+// }
+
+// let firstName = "Ravi";
+// let lastName = "Patel";
+// let message = "Hello";
+
+// let outputMessage = message + ", " + firstName + ", " + lastName;
+// console.log(outputMessage);
+
+// let sum=3 - 3 + "30";
+// console.log(sum)
+
+//typechecking
+// console.log(typeof("sum"));
+
+// Implicit type
+// String to number
+
+// console.log(a);
+// var a="30";
+// console.log(a);
+// {
+//     var a="50";
+//     console.log(a);
+//     console.log(message());
+//     function message(){
+//         console.log("Hello FSD class");
+//     }
+// }
+// console.log(a);
+
+
+// let numToStr =String (1234);
+// console.log(numToStr+""+typeof(numToStr));
+
+// let numToBool =Boolean(12.4);
+// console.log(numToBool);
+
+
+// let rows = 5;
+// for (let i = 1; i <= rows; i++) {
+//   let pattern = "";
+//   let columns = 2 * i - 1; 
+//   for (let s = 1; s <= rows - i; s++) {
+//     pattern += " ";
+//   }
+
+//   for (let j = 1; j <= columns; j++) {
+//     pattern += "*";
+//   }
+
+//   console.log(pattern);
+// }
+
+
+// let rows = 4;
+// let num = 1;
+// for (let i = 0; i <= rows; i++) {
+//   let pattern = "";
+//   let columns = 2 +i - 1;
+//   for (let s = 1; s <= rows - i; s++) {
+//     pattern += " ";
+//   }
+//   for (let j = 1; j <= columns; j++) {
+//     pattern += num + " ";
+//     num++;
+//   }
+//   console.log(pattern);
+// }
+
+
